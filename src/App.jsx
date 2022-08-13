05c5df78670d728414af81ed5975e192fd3f549d
Gianluca Cognigni <cog.gianluca@gmail.com>
2022-08-13 07:37:00 +0200

    initial setup to read .env configs and test landing page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import {useAddress, useMetamask} from "@thirdweb-dev/react";
+
 const App = () => {
+
+    const address = useAddress();
+    const connectWithMetamask = useMetamask();
+    console.log("👋 Address: ". address);
+
+
+    if(!address){
+        return <div className="landing">
+           <h1>Hey, seems your wallet is not connected! </h1>
+            <button onClick={connectWithMetamask} className="btn-hero">Connect Wallet</button>
+        </div>
+    }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Welcome to SunnyDAO</h1>
     </div>
   );
 };
